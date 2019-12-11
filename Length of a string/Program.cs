7084db70bad7e9b8ee1bf0513d319b2dda07c44a
Sachin Gurung <saching061@hotmail.com>
2019-12-10 22:50:07 -0800

    First commit

--- Length of a string/Program.cs
+++ Length of a string/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Length_of_a_string
+public class Exercise2
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        string str; /* Declares a string of size 100 */
+        int l = 0;
+
+        Console.Write("\n\nFind the length of a string :\n");
+        Console.Write("---------------------------------\n");
+        Console.Write("Input the string : ");
+        str = Console.ReadLine();
+
+        foreach (char chr in str)
         {
+            l += 1;
+
         }
+
+        Console.Write("Length of the string is : {0}\n\n", l);
     }
 }
+/*
+
+Find the length of a string :
+---------------------------------
+Input the string : a;lkjdflaksjflakjdflaksjdf
+Length of the string is : 26
+
+Press any key to continue . . .
+*/
